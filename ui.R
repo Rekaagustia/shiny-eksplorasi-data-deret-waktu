e8da13b03f5aa293ab6e5ef4c3a75e2d7b89ce57
Reka Agustia Astari <39205545+Rekaagustia@users.noreply.github.com>
2023-12-10 20:52:54 +0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -31,17 +31,19 @@ shinyUI(navbarPage(
       tags$ol(
         tags$li("Data Tren"),
         tags$p("Tren merupakan perubahan jangka panjang baik naik maupun turun dalam data. Dalam pembicaraan tentang tren kita harus memperhatikan berapa banyak data yang ada dan juga penilain kita terhadap definisi jangka panjang. Sebagai contoh peubah-peubah keadaan cuaca biasanya memberikan variasi siklus pada periode yang sangat panjang, misalkan 75 tahun. Jika kita hanya punya data untuk 20 tahun saja, maka pola osilasi jangka panjang ini akan terlihat sebagai tren."),
+        tags$p("Contoh data tren adalah jumlah penduduk, pertumbuhan tinggi pohon,jumlah pengguna internet, dan ketersediaan sumber daya alam ."),
         tags$br(),
         tags$li("Data Siklus"),
         tags$p("Pola siklus muncul apabila data dipengaruhi fluktuasi jangka panjang yang biasanya berbentuk osilasi, misalnya gelombang sinus. Gejala-gejala fisika seperti gelombang tsunami yang terjadi setiap 100 tahun sekali, jumlah titik matahari Wolfer ( ¨ Wolfer sunspot¨) biasanya membentuk suatu siklus. Perbedaan utama pola musiman dan siklus terletak pada panjang dan periodenya. Pada musiman pola cenderung memiliki panjang konstan dan terjadi berulang pada periode teratur; namun, pada siklus pola ini memiliki panjang yang bervariasi dan magnitudo yang juga bervariasi."),
         tags$br(),
         tags$li("Data Seasonal"),
         tags$p("Pola musiman muncul apabila deret dipengaruhi oleh faktor-faktor musiman, misalnya kuartalan, bulanan, harian, dan tahunan."),
+        tags$p("Contoh data seasonal adalah penjualan peralatan sekolah, produksipadi, penjualan tiket wisata."),
         tags$br(),
-        tags$li("Data Random(Fluktuatif tak beraturan"),
-        tags$p("Fluktuasi acak dalam data yang tidak dapat dijelaskan oleh tren, musiman, atau siklus. Fluktuasi ini bisa disebabkan oleh faktor-faktor acak atau faktor tak terduga lainnya.")
-      ),
-      
+        tags$li("Data Random (fluktuatif tak beraturan)"),
+        tags$p("Fluktuasi acak dalam data yang tidak dapat dijelaskan oleh tren, musiman, atau siklus. Fluktuasi ini bisa disebabkan oleh faktor-faktor acak atau faktor tak terduga lainnya."),
+        tags$p("Contoh data random adalah inflasi, harga jual saham dan harga emas.")
+        ),
     )
   ),
   # second tab panel
@@ -51,12 +53,12 @@ shinyUI(navbarPage(
              sidebarPanel(
                fluidRow(
                  column(6, selectInput(inputId = "data", label = "Pilih Data",
-                                       choices = c("Unggah Data", 
-                                                   "Masukan Mandiri"))
-                 ),
+                                       choices = c("Masukan Mandiri", 
+                                                   "Contoh Dataset")),
+                        ),
                  #Upload File
                  column(6, conditionalPanel(
-                   condition = "input.data == 'Unggah Data'",
+                   condition = "input.data == 'Masukkan Mandiri'",
                    fileInput(inputId = 'chosen_file', 
                              label = 'Unggah File CSV',
                              accept = c('text/csv',
@@ -67,27 +69,8 @@ shinyUI(navbarPage(
                actionButton("loadBtn", "Tampilkan Plot"),
                #  tableOutput("uploadData"),
                
-               #Upload mandiri
-               fluidRow(
-                 column(12, conditionalPanel(
-                   condition = "input.data == 'Masukan Mandiri'",
-                   rHandsontableOutput(outputId = "tabelle"),
-                   actionButton("save",label = "Save Data")
-                 ))
-               ),
-               # ------ Display Table ------ #
-               fluidRow(
-                 card(
-                   card_header(h5(HTML("<b>Tabel Data yang Digunakan</b>"), 
-                                  style="text-align:center")),
-                   height = 380,
-                   style = "resize:vertical;",
-                   card_body(
-                     max_height = 380,
-                     tableOutput("show_tbl")
-                   )
-                 )
-               ),
+               # ------ Display Table ------ #                      
+
                
                # ------ Set Used Variables ------ #
                fluidRow(
@@ -95,59 +78,46 @@ shinyUI(navbarPage(
                  column(6, uiOutput('dv'))  # Set Y-Variable
                ),
                
-               # ----- Refresh Button
-               conditionalPanel(
-                 condition = "input.data != 'Input Mandiri'",
-                 actionButton("refresh", "Refresh")
-               ),
-               
+
                # ----- Pilih Pola Data
-               selectInput("tipe", label = "Pilih Pola data", 
-                           choices = c("Data Trend", "Data Musiman ", "Data Siklus","Data Fluktuatif" )),
-               dateInput("start", 
-                         label = "Tanggal Awal Analisis:",
-                         value = "2019-01-01"),
-               dateInput("end", 
-                         label = "Tanggal Akhir Analisis:",
-                         value = "2023-10-30"),
+               selectInput("tipe", label = "Jenis Pola data", 
+                           choices = c("Data Trend", "Data Musiman", "Data Siklus","Data Fluktuatif" )),
                sliderInput("slider.n", label = "Smooth Trend", min = 1, max = 50, value = 30)
              ),
 
              mainPanel(
-               plotlyOutput("uploadData"),
-               plotlyOutput("myPlot")
+               plotlyOutput("myPlot"),
+               plotlyOutput("uploadData")
              )
            )
   ),
 
-  # third tab panel
+# third tab panel
   tabPanel("Forecast Plot",
            sidebarLayout(
              sidebarPanel(
-               dateInput("start2", 
-                         label = "Tanggal awal analisis:",
-                         value = "2019-01-01"),
-               dateInput("end2", 
-                         label = "Tanggal akhir analisis:",
-                         value = "2023-01-01"),
-               selectInput("type2",
-                           label = "Pilih pola data:",
-                           choices = c("Data Trend", "Data Musiman ", "Data Siklus","Data Fluktuatif" )),
-               checkboxInput("showgrid", label = "Show Grid", value = TRUE),
-               numericInput("predict",
-                            label = "Masukkan banyaknya prediksi:",
-                            value = 30),
+               sliderInput("n",
+                           "Rentang Waktu",
+                           value = c(30, 80),
+                           min = 1,
+                           max = 257
+               ),
+               # days for prediction ahead
+               numericInput("arm", "Banyaknya Peramalan", value = 10),
+               
+               # add options for prediction method
+               radioButtons("model", "Pilih Model",
+                            choices = c("ARIMA"),
+                            choiceValues = "ARIMA")
+               
              ),
              
-             # Shows second plot from server file
+             # Show a plot of the generated distribution
              mainPanel(
-               dygraphOutput("dygraph"))
+               plotOutput("trendPlot")
+               
+             )
            )
   )
 )
-),
-#-----------------FOOTER-----------------#
-footer = dashboardFooter(
-  left = "by Kelompok 2",
-  right = "IPB University, 2023"
 )
